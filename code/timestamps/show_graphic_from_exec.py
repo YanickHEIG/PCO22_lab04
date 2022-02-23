d299fb722fdc654e8a00b02aa32877a60d570639
act-reds <arzur.cateltorres@heig-vd.ch>
2022-02-23 08:27:24 +0100

    modif script timestamps

--- code/timestamps/show_graphic_from_exec.py
+++ code/timestamps/show_graphic_from_exec.py
@@ -20,6 +20,7 @@ ylab = 'Thread ID'
 figure = None
 
 if(timestamps_datas_np.size/2 > 120):
+
     figure, axs = plt.subplots(2, 1, figsize=[11, 9])
     axs[0].set_title("Ensemble de l'exécution")
     axs[0].plot(y,x)
@@ -27,14 +28,17 @@ if(timestamps_datas_np.size/2 > 120):
     axs[1].set_title("Zoom sur 50 valeurs")
     axs[1].plot(y[4:54], x[4:54])
     axs[1].yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    figure.set_figheight(8)
+    figure.set_figwidth(13)
     
 else:
-    plt.plot(timestamps_ns, timestamps_threads)
+    plt.title('THREADS CONTEXT SWITCHES', fontsize=15, fontweight='bold')
+    plt.plot(y, x)
 
 plt.xlabel(xlab, fontsize=12, fontweight='bold')
 plt.ylabel(ylab, fontsize=12, fontweight='bold')
 
-figure.suptitle('THREADS CONTEXT SWITCHES', fontsize=15, fontweight='bold')
-figure.set_figheight(8)
-figure.set_figwidth(13)
+
+
 plt.show()
